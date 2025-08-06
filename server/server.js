4e462989a286e98787ee6a6b2ed11d6fabfaf955
Ishant-179 <rajishant128@gmail.com>
2025-08-06 19:21:29 +0530

    backend cors error fixed

--- server/server.js
+++ server/server.js
@@ -14,8 +14,12 @@ connectDB(); // Connect to MongoDB
 const app = express();
 
 // Middleware
-app.use(cors(
-));
+app.use(cors({
+    origin: ["https://event-organiser-beige.vercel.app",
+        "http://localhost:5173",
+    ],  
+    credentials: true
+}));
 // Yahan hum Express body-parser ki limit badha rahe hain taaki badi files upload ho sakein.
 // isliye limit ko 50mb tak badha diya gaya hai.
 app.use(express.json({ limit: '50mb' }));
