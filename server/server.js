ebc91b1ed78b00017456f547d80837bab0bf0561
Ishant-179 <rajishant128@gmail.com>
2025-08-18 17:38:53 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -14,14 +14,8 @@ connectDB(); // Connect to MongoDB
 const app = express();
 
 // Middleware
-app.use(cors({
-    origin: ["https://event-organiser-beige.vercel.app",
-        "http://localhost:5173",
-    ],  
-    credentials: true
-}));
-// Yahan hum Express body-parser ki limit badha rahe hain taaki badi files upload ho sakein.
-// isliye limit ko 50mb tak badha diya gaya hai.
+app.use(cors());
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
