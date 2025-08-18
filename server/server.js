a49c157b647bb235aea7c040adc82d15c65dca58
Ishant-179 <rajishant128@gmail.com>
2025-08-18 17:55:15 +0530

    ch

--- server/server.js
+++ server/server.js
@@ -14,7 +14,14 @@ connectDB(); // Connect to MongoDB
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+    origin: ["https://event-organiser-beige.vercel.app",
+        "http://localhost:5173"
+    ],  
+    credentials: true
+} 
+));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
